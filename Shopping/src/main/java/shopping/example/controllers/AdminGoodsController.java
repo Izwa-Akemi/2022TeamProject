3f917388be7797f38098c9c7584856e74ec253d6
songoflove <su_nne@hotmail.com>
2022-12-09 18:35:15 +0900

    admin goods controller

---
+++ Shopping/src/main/java/shopping/example/controllers/AdminGoodsController.java
@@ -0,0 +1,18 @@
+package shopping.example.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminGoodsController {
+    private static final Logger log = LoggerFactory.getLogger(AdminGoodsController.class);
+
+    @RequestMapping("goods")
+    public String goodsLists(){
+        log.debug("view goods list");
+        return "admin_goodsList";
+    }
+}
